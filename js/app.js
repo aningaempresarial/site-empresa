aa7bb7bd2d9cc5f81b8f06f3bc54ad1ed6d4ff63
Sabrina Cristan <sabrinacristan@gmail.com>
2024-08-05 21:15:37 -0300

    🎨 Estilizando página

--- js/app.js
+++ js/app.js
@@ -1,91 +1,20 @@
-// window.addEventListener("scroll", function(){
-//     let nav = document.querySelector('#nav')
-//     nav.classList.toggle("rolagem", window.scrollY > 0)
-// })
-  
-// //step 1: get DOM
-// let nextDom = document.getElementById('next');
-// let prevDom = document.getElementById('prev');
+// Navbar que gruda
+window.onscroll = () => {stickyNav()};
 
-// let carouselDom = document.querySelector('.carousel');
-// let SliderDom = carouselDom.querySelector('.carousel .list');
-// let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
-// let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
-// let timeDom = document.querySelector('.carousel .time');
+var navbar = document.querySelector('.navbar');
+var logoNav = document.querySelector('.logo-img');
 
-// thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-// let timeRunning = 3000;
-// let timeAutoNext = 7000;
-
-// nextDom.onclick = function(){
-//     showSlider('next');    
-// }
-
-// prevDom.onclick = function(){
-//     showSlider('prev');    
-// }
-// let runTimeOut;
-// let runNextAuto = setTimeout(() => {
-//     next.click();
-// }, timeAutoNext)
-// function showSlider(type){
-//     let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
-//     let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
-    
-//     if(type === 'next'){
-//         SliderDom.appendChild(SliderItemsDom[0]);
-//         thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-//         carouselDom.classList.add('next');
-//     }else{
-//         SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
-//         thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
-//         carouselDom.classList.add('prev');
-//     }
-//     clearTimeout(runTimeOut);
-//     runTimeOut = setTimeout(() => {
-//         carouselDom.classList.remove('next');
-//         carouselDom.classList.remove('prev');
-//     }, timeRunning);
-
-//     clearTimeout(runNextAuto);
-//     runNextAuto = setTimeout(() => {
-//         next.click();
-//     }, timeAutoNext)
-// }
-
-let items = document.querySelectorAll('.slider .item');
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
-
-let active = 3;
-function loadShow(){
-    let stt = 0;
-    items[active].style.transform = `none`;
-    items[active].style.zIndex = 1;
-    items[active].style.filter = 'none';
-    items[active].style.opacity = 1;
-    for(var i = active + 1; i < items.length; i++){
-        stt++;
-        items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
-        items[i].style.zIndex = -stt;
-        items[i].style.filter = 'blur(5px)';
-        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+function stickyNav() {
+    if (window.scrollY > 0) {
+        navbar.classList.add("sticky");
+        setTimeout(() => {
+            logoNav.src = 'images/logo-sticky.png';
+        }, 1000);
+        
+    } else {
+        navbar.classList.remove("sticky");
+        setTimeout(() => {
+            logoNav.src = 'images/logo-aninga.png';
+        }, 1000);
     }
-    stt = 0;
-    for(var i = active - 1; i >= 0; i--){
-        stt++;
-        items[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`;
-        items[i].style.zIndex = -stt;
-        items[i].style.filter = 'blur(5px)';
-        items[i].style.opacity = stt > 2 ? 0 : 0.6;
-    }
-}
-loadShow();
-next.onclick = function(){
-    active = active + 1 < items.length ? active + 1 : active;
-    loadShow();
-}
-prev.onclick = function(){
-    active = active - 1 >= 0 ? active - 1 : active;
-    loadShow();
 }
